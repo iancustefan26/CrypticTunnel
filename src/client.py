96cc5cc0fc59d34745512b2d6fbddc86dcdf3813
iancustefan26 <stefaniancu46@gmail.com>
2024-06-17 13:24:09 +0300

    made the server to recive messages from the client(message, name, addres etc.) some little bugs are showing

--- src/client.py
+++ src/client.py
@@ -5,19 +5,35 @@ from usable import is_server_running
 import time
 
 
-def connect_to_server(server_addr, server_port):
+def connect_to_server(server_addr, server_port, name):
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_sock.connect((server_addr, server_port))
     print(f"Connect to the server -- IP : {server_addr} -- PORT : {server_port}")
+    print("Starting chat session...")
+    try:
+        client_sock.sendall(name.encode('utf-8'))
+        #server_name = client_sock.recv(1024).decode('utf-8')
+        #print(f"You ar talking to {server_name}")
+    except:
+        print("Error")
+    time.sleep(2)
+    clear_screen()
     while True:
-        #the send message loop
-        break
+        client_message = name + ": " + input(f"{name}: ")
+        if client_message[len(name) + 2:] == "/quit":
+            client_sock.sendall("disconnected".encode('utf-8'))
+            break
+        try:
+            client_sock.sendall(client_message.encode('utf-8'))
+        except:
+            print("Host has disconnected; try /quit")
     client_sock.close()
     print("Disconnected from the server")
 
 
 def main():
     clear_screen()
+    name = input("Type your nickname: ")
     server_addres = input("Please type the server's IP addres: ")
     server_port = input("Please type the server's PORT: ")
     server_port = int(server_port)
@@ -27,7 +43,7 @@ def main():
         main()
     else:
         print(f"Attempting to connect to server IP : {server_addres} with PORT : {server_port}")
-        connect_to_server(server_addres, server_port)
+        connect_to_server(server_addres, server_port, name)
 
 if __name__ == "__main__":
     main()
--- src/server.py
+++ src/server.py
@@ -1,11 +1,37 @@
 import socket
 import threading
 
-def listen_client(client_sock, client_addr):
+def listen_client(client_sock, client_addr, name):
     print(f"Accepted request from Client with IP : {client_addr}")
+    try:
+        client_name = client_sock.recv(1024).decode('utf-8')
+        print(f"{client_name} has joined the chat.")
+        #client_sock.sendall(name.encode('utf-8'))
+    except ConnectionResetError:
+            print(f"Connection from {client_addr} has been reset.")
+    
+    while True:
+        #your_message = input(f"{name}: ")
+
+        try:
+            # Receive data from the client
+            data = client_sock.recv(1024)
+            if not data:
+                continue
+            message = data.decode('utf-8')
+            if(message == "disconnected"):
+                print(f"{client_name} has left the chat.")
+                continue
+            print(f"{message}")
+        except ConnectionResetError:
+            print(f"{client_name} has left the chat.")
+            break
+
+    client_sock.close()
+             
     
 
-def start_server():
+def start_server(name):
     host = "0.0.0.0"
     port = 12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,8 +41,9 @@ def start_server():
 
     while True:
         client_sock, client_addr = server_socket.accept()
-        threading.Thread(target=listen_client, args=(client_sock, client_addr)).start()
+        threading.Thread(target=listen_client, args=(client_sock, client_addr, name)).start()
 
 
 if __name__ == "__main__":
-    start_server()
+    name = input("Type your nickname: ")
+    start_server(name)
