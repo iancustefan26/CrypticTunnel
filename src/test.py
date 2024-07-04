27adffb356f0f831af50f71a65d528b795aca151
iancustefan26 <stefaniancu46@gmail.com>
2024-07-04 11:01:20 +0300

    Removed more unnecesary files and now all is clean and working

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def main():
-    response = requests.get('https://www.example.com')
-    print('Response Status Code:', response.status_code)
-
-if __name__ == '__main__':
-    main()
